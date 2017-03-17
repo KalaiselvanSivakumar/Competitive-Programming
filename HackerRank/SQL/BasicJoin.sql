01e059ff01e96e5b0555220b696132e5da469592
Kalai selvan Sivakumar <kalaiselvan1993@ymail.com>
2017-03-17 15:17:18 +0530

    Update BasicJoin.sql

--- HackerRank/SQL/BasicJoin.sql
+++ HackerRank/SQL/BasicJoin.sql
@@ -8,3 +8,13 @@ Asian Population:-
 
     Oracle:
     SELECT SUM(city.population) FROM city INNER JOIN country ON city.countrycode = country.code WHERE country.continent = 'Asia';
+
+
+The Report:-
+    MySQL:
+    SELECT CASE WHEN Grades.Grade >= 8 THEN Students.Name ELSE 'NULL' END, Grades.Grade, Students.Marks FROM Students JOIN Grades ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark ORDER BY Grades.Grade DESC, Students.Name ASC;
+    
+    Oracle:
+    SELECT CASE WHEN Grades.Grade >= 8 THEN Students.Name ELSE 'NULL' END, Grades.Grade, Students.Marks FROM Students JOIN Grades ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark ORDER BY Grades.Grade DESC, Students.Name ASC;
+
+
